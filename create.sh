9d63df94a484675b76097e2dd920b9be19069fd0
Julian Fleischer <julian@thirdhash.com>
2019-01-28 22:50:26 +0100

    Run container privileged to allow debugging with gdb

--- create.sh
+++ create.sh
@@ -5,6 +5,7 @@ NAME=${1:-"bitcoin-dev"}
 docker run \
     --name ${NAME} \
     -it \
+    --privileged=true \
     -v ${HOME}/.ssh:/root/.ssh:ro \
     -v ${HOME}:/host:rw \
     julianfleischer/bitcoin-dev:ubuntu-18.04
